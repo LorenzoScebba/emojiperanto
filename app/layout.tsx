4804474002cfa7bc3cd6efbe0c76f6e3e2aaf6ac
Lorenzo Scebba <lorenzo@scebba.com>
2023-07-12 21:10:49 +0200

    PWATHA! (#26)

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials"></link>
       </head>
       <body>
         <MantineProvider theme={theme} defaultColorScheme={"dark"}>
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,56 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 import { Box, Button } from "@mantine/core";
 
+const emojis = [
+  "😀",
+  "😁",
+  "😂",
+  "🤣",
+  "😃",
+  "😄",
+  "😅",
+  "😆",
+  "😉",
+  "😊",
+  "😋",
+  "😎",
+  "😍",
+  "😘",
+  "🥰",
+  "😗",
+  "😢",
+  "😭",
+  "😦",
+  "😧",
+  "😨",
+  "😩",
+  "🤯",
+  "😬",
+  "😰",
+  "😱",
+  "🥵",
+  "🥶",
+  "😳",
+  "🤪",
+  "😵",
+  "🥴",
+];
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const Header = () => {
+  const [emoji] = useState(emojis[getRandomInt(0, emojis.length - 1)])
+
   return (
     <Box component={"header"} className={styles.header}>
-      <Box className={styles.title}>Emojiperanto</Box>
+      <Box className={styles.title} suppressHydrationWarning>Emojiperanto {emoji}</Box>
       <Box className={styles.subtitle}>
         A <u>NSFW</u> emoji guide to express concepts.
       </Box>
@@ -17,7 +60,7 @@ const Header = () => {
           onClick={() => {
             window.open(
               "https://github.com/LorenzoScebba/emojiperanto",
-              "_blank",
+              "_blank"
             );
           }}
         >
@@ -28,7 +71,7 @@ const Header = () => {
           onClick={() => {
             window.open(
               "https://github.com/LorenzoScebba/emojiperanto/blob/master/CONTRIBUTORS.md",
-              "_blank",
+              "_blank"
             );
           }}
         >
