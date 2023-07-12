8f8ce93ee97f8bbf1af8994f35b8bb906ade8e77
Lorenzo Scebba <lorenzo@scebba.com>
2023-07-12 09:55:22 +0000

    [👉👈] Can we pls?

--- data/emojis.ts
+++ data/emojis.ts
@@ -569,6 +569,32 @@ const data: IEmoji[] = [
       },
     },
   },
+  {
+    id: "importantdecisions",
+    emoji: "🙋⬆️👉⬇️❔",
+    title: ":raising_hand: :arrow_up: :point_right: :arrow_down: :question:",
+    description: "I'll be top, you'll be bottom?",
+    translations: {
+      it: {
+        description: "Io sopra, tu sotto?"
+      },
+      de: {
+        description: "Ich oben, du unten?",
+      },
+      fr: {
+        description: "Je suis au dessus, tu es en dessous?",
+      },
+      es: {
+        description: "¿Yo arriba, tú abajo?",
+      },
+      it_ts: {
+        description: "Me in coppa, tu de sotto?",
+      },
+      it_bg: {
+        description: "Me süra, te söta?",
+      },
+    }
+  }
 ];
 
 export default data;
