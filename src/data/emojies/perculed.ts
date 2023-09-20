fef5287529ce8d2395602677e933b5dbb70ef6ee
Lorenzo Scebba <lorenzo.scebba@outlook.com>
2023-09-20 13:10:22 +0200

    Perculed

---
+++ src/data/emojies/perculed.ts
@@ -0,0 +1,37 @@
+import { IEmoji } from "../emojis.ts";
+
+export default {
+  id: "perculed",
+  emoji: ["🤣👉🧑"],
+  description: "You got mocked.",
+  translations: {
+    it: {
+      description: "Sei stato perculato.",
+      emoji: ["🍐➡🍑"],
+    },
+    de: {
+      description: "Du wurdest verspottet.",
+    },
+    fr: {
+      description: "Tu as été moqué.",
+    },
+    es: {
+      description: "Te burlaron.",
+    },
+    it_ts: {
+      description: "Sei stato perculato.",
+      emoji: ["🍐➡🍑"],
+    },
+    it_bg: {
+      description: "Sei stato perculato.",
+      emoji: ["🍐➡🍑"],
+    },
+    meow: {
+      description: "Mrrr meow pfff meow!!",
+    },
+    kawaii: {
+      description:
+        "Chu got mocked, nya~! But don't get dwowned in sorrow, find stwength and gow onn~!",
+    },
+  },
+} as IEmoji;
